c447f0457398bafe0878bbde33cfc932ffe9f4a8
Anders Haig <andershaig@gmail.com>
2014-07-06 20:32:51 -0700

    Added a custom search filter currently only for title

--- js/filters.js
+++ js/filters.js
@@ -1,10 +1,29 @@
 'use strict';
 
 /* Filters */
+angular.module('marg.filters', [])
+.filter('textSearch', function () {
+  return function (items, search) {
+    // console.log(items);
+    // console.log(search);
 
-angular.module('marg.filters', []).
-filter('interpolate', ['version', function (version) {
-  return function(text) {
+    if (!search) {
+      return items;
+    }
+
+    var queryStr = search.query;
+    if (!queryStr || '' === queryStr) {
+      return items;
+    }
+
+    return items.filter(function (element, index, array) {
+      var title = element.title.toLowerCase();
+      return title.indexOf(queryStr.toLowerCase()) !== -1;
+    });
+  }
+})
+.filter('interpolate', ['version', function (version) {
+  return function (text) {
     return String(text).replace(/\%VERSION\%/mg, version);
   }
 }]);
